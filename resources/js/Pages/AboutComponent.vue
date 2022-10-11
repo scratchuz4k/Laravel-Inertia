516b0266147b835a8d244ace826c8a9f9fd3563e
Marcos Ferreira <marcos_ferreira@outlook.pt>
2022-10-11 21:03:51 +0100

    install app

---
+++ resources/js/Pages/AboutComponent.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+
+        <Head title="Dashboard" />
+        <h1 class="mb-8 text-3xl font-bold">About</h1>
+        <Link class="mt-1" href="/">
+        back
+        </Link>
+    </div>
+</template>ç
+  
+<script>
+import { Head } from '@inertiajs/inertia-vue3'
+import { Link } from '@inertiajs/inertia-vue3'
+export default {
+    components: {
+        Head,
+        Link
+    },
+}
+</script>
---
+++ resources/js/Pages/Dashboard/Index.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <Head title="Dashboard" />
+        <h1 class="mb-8 text-3xl font-bold">Dashboard</h1>
+        <p class="mb-8 leading-normal">Hey there! Welcome to Ping CRM, a demo app designed to help illustrate how <a
+                class="text-indigo-500 hover:text-orange-600 underline" href="https://inertiajs.com">Inertia.js</a>
+            works.</p>
+        <Link class="mt-1" href="/about">
+        about
+        </Link>
+    </div>
+</template>
+  
+<script>
+import { Head } from '@inertiajs/inertia-vue3'
+//   import Layout from '@/Shared/Layout'
+import { Link } from '@inertiajs/inertia-vue3'
+export default {
+    components: {
+        Head,
+        Link,
+    },
+    // layout: Layout,
+}
+</script>
